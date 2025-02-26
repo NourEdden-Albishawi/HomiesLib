30f8e72c3253183aa6d5fa3706e233dcf3d115f6
Nour Edden AlBishawi <noureddenbishawi@gmail.com>
2025-02-27 02:01:04 +0300

    Introduced LibEvents for abstracted event handling

---
+++ src/main/java/dev/al3mid3x/lib/events/LibEvent.java
@@ -0,0 +1,13 @@
+package dev.al3mid3x.lib.events;
+
+public abstract class LibEvent {
+    private boolean cancelled = false;
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
