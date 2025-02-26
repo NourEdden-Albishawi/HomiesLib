16e62f46a7718f773d54ec316158e45833f1d003
Nour Edden AlBishawi <noureddenbishawi@gmail.com>
2025-02-27 01:58:19 +0300

    Refactored LibMenu to integrate HomiesEventBus

--- src/main/java/dev/al3mid3x/lib/gui/LibMenu.java
+++ src/main/java/dev/al3mid3x/lib/gui/LibMenu.java
@@ -1,27 +1,31 @@
 package dev.al3mid3x.lib.gui;
 
-import dev.al3mid3x.lib.utils.ItemFactory;
+import dev.al3mid3x.lib.HomiesLib;
+import dev.al3mid3x.lib.events.HomiesEventBus;
+import dev.al3mid3x.lib.events.menus.MenuClickEvent;
+import dev.al3mid3x.lib.events.menus.MenuCloseEvent;
+import dev.al3mid3x.lib.events.menus.MenuOpenEvent;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 import java.util.UUID;
 
 @Getter
-public abstract class LibMenu {
+public abstract class LibMenu implements Listener {
     private final UUID uniqueId = UUID.randomUUID();
-
     private final Inventory inventory;
-
     private final String name;
-
     private Player playerWhoOpenedInventory;
-
     private int page = 0;
-
+    private final HomiesEventBus eventBus;
 
     protected abstract int getMaxItemsPerPage();
 
@@ -29,47 +33,37 @@ public abstract class LibMenu {
 
     public abstract List<LibItemMenu> getItems();
 
-    public LibMenu(String name, int size) {
+    public LibMenu(String name, int size, HomiesEventBus eventBus) {
         this.name = name;
         this.inventory = Bukkit.createInventory(null, size, name);
+        this.eventBus = eventBus;
+        Bukkit.getPluginManager().registerEvents(this, HomiesLib.getPlugin(HomiesLib.class));
     }
 
-    public void consume(InventoryClickEvent event) {
-        for (LibItemMenu item : getItems())
-            item.getConsumer().accept(event, new ItemFactory(event.getCurrentItem()));
+    public void open(Player player) {
+        this.playerWhoOpenedInventory = player;
+        eventBus.fireEvent(new MenuOpenEvent(player, this));
+        player.openInventory(inventory);
     }
 
-    public void build() {
-        if (isPaginated()) {
-            for (int i = 0; i < getMaxItemsPerPage(); i++) {
-                int index = getMaxItemsPerPage() * this.page + i;
-                if (index >= getItems().size())
-                    break;
-                for (LibItemMenu item : getItems())
-                    this.inventory.setItem(item.getSlot(), item.getItem().complete());
-            }
-            return;
-        }
-        for (LibItemMenu item : getItems())
-            this.inventory.setItem(item.getSlot(), item.getItem().complete());
+    public void setItem(int slot, ItemStack item) {
+        inventory.setItem(slot, item);
     }
 
-    public void openInventory(Player player) {
-        player.openInventory(this.inventory);
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player player)) return;
+        if (!event.getInventory().equals(this.inventory)) return;
+        event.setCancelled(true);
+
+        eventBus.fireEvent(new MenuClickEvent(player, this, event.getSlot(), event.getCurrentItem()));
     }
 
-    public void Next() {
-        if (getItems().size() >= getMaxItemsPerPage()) {
-            this.page++;
-            openInventory(getPlayerWhoOpenedInventory());
-        }
-    }
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (!(event.getPlayer() instanceof Player player)) return;
+        if (!event.getInventory().equals(this.inventory)) return;
 
-    public void Previous() {
-        if (this.page != 0) {
-            this.page--;
-            openInventory(getPlayerWhoOpenedInventory());
-        }
+        eventBus.fireEvent(new MenuCloseEvent(player, this));
     }
-
 }
