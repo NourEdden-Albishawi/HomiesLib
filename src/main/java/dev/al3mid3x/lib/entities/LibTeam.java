dc25b2ca50c77f1e1cb86afb365e975caa57dc19
Nour Edden AlBishawi <noureddenbishawi@gmail.com>
2025-02-27 23:44:33 +0300

    Enhance LibTeam with member and moderator management
    - Added `addMember(UUID uuid)` to add a player to the team
    - Added `removeMember(UUID uuid)` to remove a player from the team (fixed incorrect addition)
    - Added `promote(UUID uuid)` to assign a player as a moderator
    - Added `demote(UUID uuid)` to remove a player from the moderator role

--- src/main/java/dev/al3mid3x/lib/entities/LibTeam.java
+++ src/main/java/dev/al3mid3x/lib/entities/LibTeam.java
@@ -13,4 +13,20 @@ public class LibTeam {
     private final UUID founderId;
     private final List<UUID> moderators = Lists.newArrayList();
     private final List<UUID> members = Lists.newArrayList();
+
+    public void addMember(UUID uuid) {
+        this.members.add(uuid);
+    }
+
+    public void promote(UUID uuid) {
+        this.moderators.add(uuid);
+    }
+
+    public void removeMember(UUID uuid) {
+        this.members.add(uuid);
+    }
+
+    public void demote(UUID uuid) {
+        this.moderators.remove(uuid);
+    }
 }
