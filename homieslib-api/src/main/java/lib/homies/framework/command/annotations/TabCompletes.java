2fbd59e5294e15e76a57ab0558bf67501dc65040
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-10 09:29:06 +0300

    feat(command): Introduce @TabCompletes container annotation
        Created the `@TabCompletes` annotation as a container for multiple
        `@TabComplete` annotations. This is necessary to make `@TabComplete`
        repeatable in Java.

---
+++ homieslib-api/src/main/java/lib/homies/framework/command/annotations/TabCompletes.java
@@ -0,0 +1,12 @@
+package lib.homies.framework.command.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface TabCompletes {
+    TabComplete[] value();
+}
