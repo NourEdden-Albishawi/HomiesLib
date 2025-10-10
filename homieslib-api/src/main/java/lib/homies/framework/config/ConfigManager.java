86e268b76af72eb6e3f8dd4fce1b787eb882d0e2
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-10 08:59:17 +0300

    refactor(config): Standardize PluginContext parameter in ConfigManager
        Renamed `callingPluginContext` to `pluginContext` in the `loadConfig`
        and `saveConfig` methods of the `ConfigManager` interface for improved
        consistency and clarity across the API.

--- homieslib-api/src/main/java/lib/homies/framework/config/ConfigManager.java
+++ homieslib-api/src/main/java/lib/homies/framework/config/ConfigManager.java
@@ -14,23 +14,23 @@ public interface ConfigManager {
      * The configuration class must be a POJO annotated with {@link ConfigFile}.
      * Fields within the class intended for configuration should be annotated with {@link lib.homies.framework.config.annotations.ConfigKey}.
      *
-     * @param callingPluginContext The context of the plugin calling this method, used to access its data folder and resources.
+     * @param pluginContext The context of the plugin calling this method, used to access its data folder and resources.
      * @param configClass The class representing the configuration.
      * @return An instance of the configuration class populated with values from the file.
      * @param <T> The type of the configuration class.
      * @throws IllegalArgumentException if the configClass is not annotated with @ConfigFile.
      * @throws RuntimeException if loading or saving fails due to I/O or reflection errors.
      */
-    <T> T loadConfig(PluginContext callingPluginContext, Class<T> configClass);
+    <T> T loadConfig(PluginContext pluginContext, Class<T> configClass);
 
     /**
      * Saves the current state of a configuration object back to its file.
      * The configuration object must be an instance of a class annotated with {@link ConfigFile}.
      *
-     * @param callingPluginContext The context of the plugin calling this method, used to access its data folder.
+     * @param pluginContext The context of the plugin calling this method, used to access its data folder.
      * @param configInstance The configuration object to save.
      * @throws IllegalArgumentException if the configInstance's class is not annotated with @ConfigFile.
      * @throws RuntimeException if saving fails due to I/O or reflection errors.
      */
-    void saveConfig(PluginContext callingPluginContext, Object configInstance);
+    void saveConfig(PluginContext pluginContext, Object configInstance);
 }
