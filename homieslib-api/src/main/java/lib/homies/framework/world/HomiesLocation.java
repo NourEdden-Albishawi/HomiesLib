ff4e86b213a4158505c9937dd9e241f187bede87
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-10 09:00:01 +0300

    refactor(world): Simplify HomiesLocation interface and add platform access
        Removed redundant Javadoc comments from getter methods for conciseness.
        Added a generic `getAs(Class<T> platformClass)` method to allow safe
        access to the underlying platform-specific location object.

--- homieslib-api/src/main/java/lib/homies/framework/world/HomiesLocation.java
+++ homieslib-api/src/main/java/lib/homies/framework/world/HomiesLocation.java
@@ -1,45 +1,21 @@
 package lib.homies.framework.world;
 
-/**
- * A platform-agnostic representation of a location within a game world.
- * This interface abstracts away platform-specific location objects (e.g., Bukkit's Location).
- * The concrete implementation will be provided by a platform-specific module,
- * which can be unwrapped to get the underlying platform object if needed.
- */
 public interface HomiesLocation {
-    /**
-     * Gets the {@link HomiesWorld} this location is in.
-     * @return The world of this location.
-     */
+
     HomiesWorld getWorld();
-
-    /**
-     * Gets the X coordinate of this location.
-     * @return The X coordinate.
-     */
     double getX();
-
-    /**
-     * Gets the Y coordinate of this location.
-     * @return The Y coordinate.
-     */
     double getY();
-
-    /**
-     * Gets the Z coordinate of this location.
-     * @return The Z coordinate.
-     */
     double getZ();
-
-    /**
-     * Gets the yaw (horizontal rotation) of this location.
-     * @return The yaw value.
-     */
     float getYaw();
+    float getPitch();
 
     /**
-     * Gets the pitch (vertical rotation) of this location.
-     * @return The pitch value.
+     * Gets the underlying platform-specific location object.
+     * This is an escape hatch to access platform-specific APIs.
+     *
+     * @param platformClass The class of the platform-specific location object (e.g., org.bukkit.Location.class).
+     * @param <T>           The type of the platform-specific location.
+     * @return The platform-specific location object, or null if the type is incompatible.
      */
-    float getPitch();
+    <T> T getAs(Class<T> platformClass);
 }
