62100ad0c8be337a6ec6ab48a46061e5feff08f3
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-10 09:00:36 +0300

    refactor(world): Simplify HomiesWorld interface and add platform access
        Removed redundant Javadoc comments. Added a generic `getAs(Class<T> platformClass)`
        method to allow safe access to the underlying platform-specific world object.

--- homieslib-api/src/main/java/lib/homies/framework/world/HomiesWorld.java
+++ homieslib-api/src/main/java/lib/homies/framework/world/HomiesWorld.java
@@ -1,15 +1,15 @@
 package lib.homies.framework.world;
 
-/**
- * A platform-agnostic interface representing a game world.
- * This abstracts away platform-specific world objects (e.g., Bukkit's World).
- * The concrete implementation will be provided by a platform-specific module,
- * which can be unwrapped to get the underlying platform object if needed.
- */
 public interface HomiesWorld {
-    /**
-     * Gets the name of the world.
-     * @return The world's name.
-     */
     String getName();
+
+    /**
+     * Gets the underlying platform-specific world object.
+     * This is an escape hatch to access platform-specific APIs.
+     *
+     * @param platformClass The class of the platform-specific world object (e.g., org.bukkit.World.class).
+     * @param <T>           The type of the platform-specific world.
+     * @return The platform-specific world object, or null if the type is incompatible.
+     */
+    <T> T getAs(Class<T> platformClass);
 }
