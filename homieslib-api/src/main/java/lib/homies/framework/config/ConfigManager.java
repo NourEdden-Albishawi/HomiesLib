efe9b69e6aac87ce5d60b61ceeceff17156a2c92
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-12 00:51:55 +0300

    feat(config): Make ConfigManager asynchronous
        Refactored the `ConfigManager` interface to perform all file I/O
        operations asynchronously, preventing server lag from blocking the
        main thread.
    
        - **BREAKING CHANGE:** The `loadConfig` method now returns a `CompletableFuture<T>`.
        - **BREAKING CHANGE:** The `saveConfig` method now returns a `CompletableFuture<Void>`.
    
        Developers must now use methods like `.thenAccept()` to handle the
        results of configuration loading and saving.

--- homieslib-api/src/main/java/lib/homies/framework/config/ConfigManager.java
+++ homieslib-api/src/main/java/lib/homies/framework/config/ConfigManager.java
@@ -3,34 +3,33 @@ package lib.homies.framework.config;
 import lib.homies.framework.PluginContext;
 import lib.homies.framework.config.annotations.ConfigFile;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  * A platform-agnostic interface for managing configuration files.
- * Framework developers will use this service to load and save their plugin's configuration.
+ * All operations are performed asynchronously to prevent blocking the main server thread.
  */
 public interface ConfigManager {
 
     /**
-     * Loads a configuration class, creating the file with default values if it doesn't exist.
+     * Asynchronously loads a configuration class, creating the file with default values if it doesn't exist.
      * The configuration class must be a POJO annotated with {@link ConfigFile}.
      * Fields within the class intended for configuration should be annotated with {@link lib.homies.framework.config.annotations.ConfigKey}.
      *
      * @param pluginContext The context of the plugin calling this method, used to access its data folder and resources.
      * @param configClass The class representing the configuration.
-     * @return An instance of the configuration class populated with values from the file.
      * @param <T> The type of the configuration class.
-     * @throws IllegalArgumentException if the configClass is not annotated with @ConfigFile.
-     * @throws RuntimeException if loading or saving fails due to I/O or reflection errors.
+     * @return A {@link CompletableFuture} that will complete with an instance of the configuration class.
      */
-    <T> T loadConfig(PluginContext pluginContext, Class<T> configClass);
+    <T> CompletableFuture<T> loadConfig(PluginContext pluginContext, Class<T> configClass);
 
     /**
-     * Saves the current state of a configuration object back to its file.
+     * Asynchronously saves the current state of a configuration object back to its file.
      * The configuration object must be an instance of a class annotated with {@link ConfigFile}.
      *
      * @param pluginContext The context of the plugin calling this method, used to access its data folder.
      * @param configInstance The configuration object to save.
-     * @throws IllegalArgumentException if the configInstance's class is not annotated with @ConfigFile.
-     * @throws RuntimeException if saving fails due to I/O or reflection errors.
+     * @return A {@link CompletableFuture} that will complete when the save operation is finished.
      */
-    void saveConfig(PluginContext pluginContext, Object configInstance);
+    CompletableFuture<Void> saveConfig(PluginContext pluginContext, Object configInstance);
 }
